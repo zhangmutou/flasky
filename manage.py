ab98ee8237ef8cb854fbfd2352e8c2357fcf01f9
etcoder <bjzhangshuai1@corp.netease.com>
2017-11-28 19:37:41 +0800

    test

--- manage.py
+++ manage.py
@@ -15,12 +15,79 @@ if os.path.exists('.env'):
 
 from app.models import User, Follow, Role, Permission, Post, Comment, NexApiCase
 from app import db, celery, app
-from flask_script import Manager, Shell
+from flask_script import Manager, Shell, Command, Option, Server
 from flask_migrate import Migrate, MigrateCommand
 from app.workers import server_monitor_worker
 
 # register_app_blueprint(app)
 # celery = Celery(app.name)
+from gunicorn.app.base import Application
+
+class GunicornServer(Command):
+
+    description = 'Run the app within Gunicorn'
+
+    def __init__(self, host='0.0.0.0', port=8000, workers=4):
+
+        self.port = port
+        self.host = host
+        self.workers = workers
+
+    def get_options(self):
+        return (
+            Option('-H', '--host',
+                   dest='host',
+                   default=self.host),
+
+            Option('-p', '--port',
+                   dest='port',
+                   type=int,
+                   default=self.port),
+
+            Option('-w', '--workers',
+                   dest='workers',
+                   type=int,
+                   default=self.workers),
+        )
+
+    def handle(self, app, host, port, workers):
+
+        # host = kwargs['host']
+        # port = kwargs['port']
+        # workers = kwargs['workers']
+
+        def remove_non_gunicorn_command_line_args():
+            import sys
+            args_to_remove = ['--port','-p']
+            def args_filter(name_or_value):
+                keep = not args_to_remove.count(name_or_value)
+                if keep:
+                    previous = sys.argv[sys.argv.index(name_or_value) - 1]
+                    keep = not args_to_remove.count(previous)
+                return keep
+            sys.argv = filter(args_filter, sys.argv)
+
+        remove_non_gunicorn_command_line_args()
+
+        from gunicorn import version_info
+        if version_info < (0, 9, 0):
+            from gunicorn.arbiter import Arbiter
+            from gunicorn.config import Config
+            arbiter = Arbiter(Config({'bind': "%s:%d" % (host, int(port)),'workers': workers}), app)
+            arbiter.run()
+        else:
+            class FlaskApplication(Application):
+                def init(self, parser, opts, args):
+                    return {
+                        'bind': '{0}:{1}'.format(host, port),
+                        'workers': workers
+                    }
+
+                def load(self):
+                    return app
+
+            FlaskApplication().run()
+
 manager = Manager(app)
 migrate = Migrate(app, db)
 
@@ -28,8 +95,10 @@ migrate = Migrate(app, db)
 def make_shell_context():
     return dict(app=app, db=db, User=User, Follow=Follow, Role=Role,
                 Permission=Permission, Post=Post, Comment=Comment)
+
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
+manager.add_command('gunicorn', GunicornServer())
 
 
 @manager.command
